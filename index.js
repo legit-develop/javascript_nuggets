5785a31e38b09b560878c43104d45a86ae3f0cea
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-06-12 10:06:30 +0200

    setting up object keys dynamically

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ person.age = 23;
 
 //Checking for all properties
 console.log(person);
+
+//square bracket notation
+const items = {
+  "featured-items": ["item1", "item2"],
+};
+
+console.log(items["featured-items"]);
+
+console.log(person["name"]);
+
+//Setting Keys dynamically
+let appState = "loading";
+appState = "error";
+const app = {
+  [appState]: true,
+};
+
+console.log(app);
+
+//adding properties
+let keyName = "computer";
+app[keyName] = "apple";
+
+console.log(app);

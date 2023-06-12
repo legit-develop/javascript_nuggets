e9e84ebc45070c21805559cc406d70a243ef6c16
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-06-12 09:40:56 +0200

    setting ordinary object value

---
+++ index.js
@@ -0,0 +1,15 @@
+//Dynamic Object Keys
+
+//Normal object
+const person = {
+  name: "John",
+};
+
+//Retrieving name property
+console.log(person.name); // John
+
+//Adding a new property
+person.age = 23;
+
+//Checking for all properties
+console.log(person);

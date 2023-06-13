bb80271211155a913eda53e7dbd76367bb72e5da
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-06-13 15:37:17 +0200

    set up unique values

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ console.log(person.name); // John
 person.age = 23;
 
 //Checking for all properties
-console.log(person);
+//console.log(person);
 
 //square bracket notation
 const items = {
   "featured-items": ["item1", "item2"],
 };
 
-console.log(items["featured-items"]);
+//console.log(items["featured-items"]);
 
-console.log(person["name"]);
+//console.log(person["name"]);
 
 //Setting Keys dynamically
 let appState = "loading";
@@ -30,13 +30,13 @@ const app = {
   [appState]: true,
 };
 
-console.log(app);
+//console.log(app);
 
 //adding properties
 let keyName = "computer";
 app[keyName] = "apple";
 
-console.log(app);
+//console.log(app);
 //note that the property takes the value of the defined property
 
 //Example
@@ -52,16 +52,58 @@ function updateState(key, value) {
 
 //adding a value for the name property
 updateState("name", "john");
-console.log(state);
+//console.log(state);
 
 //Adding a value for the job property
 updateState("job", "computer engineer");
-console.log(state);
+//console.log(state);
 
 //Updating properties values
 updateState("loading", false);
-console.log(state);
+//console.log(state);
 
 //adding new keys
 updateState("products", ["apples", "banana"]);
-console.log(state);
+//console.log(state);
+
+//Part 2 - Unique values
+
+console.log("unique values");
+const menu = [
+  {
+    name: "pancakes",
+    category: "breakfast",
+  },
+  {
+    name: "burger",
+    category: "lunch",
+  },
+  {
+    name: "steak",
+    category: "dinner",
+  },
+  {
+    name: "bacon",
+    category: "breakfast",
+  },
+  {
+    name: "eggs",
+    category: "breakfast",
+  },
+  {
+    name: "pasta",
+    category: "dinner",
+  },
+];
+
+//Iterate over all the categories
+const categories = menu.map((item) => item.category);
+console.log(categories);
+
+//Unique categories
+const newCategories = new Set(categories);
+console.log(newCategories); //result in an object format
+
+//Unique values in an array format
+const uniqueCategory = [...new Set(categories)];
+console.log(uniqueCategory);
